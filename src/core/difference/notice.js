4075ad8ef063a9128b1ca1cea44b089d342967c6
ONOUE Origami <yonosamochi@me.com>
2017-12-16 20:09:55 +0900

    はい

---
+++ src/core/difference/notice.js
@@ -0,0 +1,27 @@
+export const Notice = {
+    type: {
+        [Services.Twitter]: '',
+        [Services.GnuSocial]: '',
+        [Services.Mastodon]: 'type',
+    },
+    id: {
+        [Services.Twitter]: '',
+        [Services.GnuSocial]: '',
+        [Services.Mastodon]: 'id',
+    },
+    createdAt: {
+        [Services.Twitter]: '',
+        [Services.GnuSocial]: '',
+        [Services.Mastodon]: 'created_at',
+    },
+    target: {
+        [Services.Twitter]: '',
+        [Services.GnuSocial]: '',
+        [Services.Mastodon]: 'status',
+    },
+    sender: {
+        [Services.Twitter]: '',
+        [Services.GnuSocial]: '',
+        [Services.Mastodon]: 'account',
+    },
+};
