9d775d5c26ae8e2690ecb709060a9841300523a5
ONOUE Origami <yonosamochi@me.com>
2017-12-16 18:38:34 +0900

    はい

---
+++ src/component/App.jsx
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react';
+import {withStyles} from 'material-ui/styles';
+
+const styles = (theme: Object): any => ({
+    root: {
+
+    }
+})
+
+const App = () => (
+    <div>
+        {'hello'}
+    </div>
+);
+
+export default withStyles(styles)(App);
