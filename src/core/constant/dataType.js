8abd598ccb24351cd0e0b0ef8aebd814bfbbc9c8
ONOUE Origami <yonosamochi@me.com>
2017-12-17 08:17:17 +0900

    降ってきたデータの属性の概念とタイムラインの属性の概念を導入

---
+++ src/core/constant/dataType.js
@@ -0,0 +1,6 @@
+// @flow
+
+export const home = 'home';
+export const activity = 'activity';
+export const directMail = 'directMail';
+export const streaming = 'streaming';
---
+++ src/core/constant/timelineType.js
@@ -0,0 +1,19 @@
+import * as dataType from './dataType';
+
+export default {
+    Home: {
+        description: 'Home Timeline',
+        api: undefined,
+        dataname: dataType.home,
+    },
+    Activity: {
+        description: 'Mentions and Reactions timeline.',
+        api: undefined,
+        dataname: dataType.activity,
+    },
+    /* 'Direct Message': {
+        description: 'Direct message timeline.',
+        api: undefined,
+        dataname: dataType.directMail,
+    },*/
+};
