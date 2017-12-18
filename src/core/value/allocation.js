13810ae1c5ac0101f3a068b5a0aae561b2137b4e
on_origami <yonosamochi@me.com>
2017-12-18 11:12:41 +0900

    未完成ですが取得したcontentやeventを振り分けて適切なオブジェクトを生成するallocation.jsを追加

--- src/core/value/allocation.js
+++ src/core/value/allocation.js
@@ -5,12 +5,13 @@ import * as dataTypes from '../constant/dataType';
 import Content from './Content';
 import Event from './Event';
 
+// TODO: 各種dataTypeに合わせて書き加えてください
 export default (service: string, dataType: string, data: Array<Object>): ?Array<any> => {
     switch (service) {
     case Services.Twitter:
         switch (dataType) {
         case dataTypes.home:
-            break;
+            return data.map((item: Object): Content => new Content(service, item));
         case dataTypes.activity:
             break;
         case dataTypes.directMail:
@@ -20,7 +21,7 @@ export default (service: string, dataType: string, data: Array<Object>): ?Array<
     case Services.GnuSocial:
         switch (dataType) {
         case dataTypes.home:
-            break;
+            return data.map((item: Object): Content => new Content(service, data));
         case dataTypes.activity:
             break;
         case dataTypes.directMail:
@@ -30,7 +31,7 @@ export default (service: string, dataType: string, data: Array<Object>): ?Array<
     case Services.Mastodon:
         switch (dataType) {
         case dataTypes.home:
-            break;
+            return data.map((item: Object): Content => new Content(service, data));
         case dataTypes.activity:
             break;
         case dataTypes.directMail:
