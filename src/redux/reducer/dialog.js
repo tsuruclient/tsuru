b1ca40c3b9e5a4d2a94bf8216400768f1c05c57f
ONOUE Origami <yonosamochi@me.com>
2017-12-19 13:27:13 +0900

    DialogをReducerでやっていく

--- src/redux/reducer/dialog.js
+++ src/redux/reducer/dialog.js
@@ -3,8 +3,21 @@
 import { handleActions } from 'redux-actions';
 import * as types from '../constant';
 
+import timelineTypes, {Home} from '../../core/constant/timelineType';
+
 const initState = {
-    // TODO: Dialog達よここに集いなさい…
+    AddAccountDialog: {
+        open: true,
+        step: 0,
+        selected: 0,
+        isCommon: false,
+        receivedError: null,
+    },
+    AddTimelineDialog: {
+        open: false,
+        selectedAccount: 0,
+        selectedTimelineType: Home,
+    },
 }
 
 export default handleActions({
