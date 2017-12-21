fca57d4738afe59a88bd1d9d7a2e2cd598f6704f
ONOUE Origami <yonosamochi@me.com>
2017-12-21 23:20:06 +0900

    開発用のデータを流し込むのをやめました

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -3,10 +3,7 @@
 import { takeEvery } from 'redux-saga/effects';
 import * as types from '../constant';
 
-import setDevData from './dev';
-
 function* rootSaga(): any {
-    yield takeEvery(types.SET_DEV_DATA, setDevData);
 }
 
 export default rootSaga;
