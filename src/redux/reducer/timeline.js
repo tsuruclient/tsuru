35af5bce31f3ae59a310daa8296838aefad2095e
ONOUE Origami <yonosamochi@me.com>
2017-12-25 02:31:34 +0900

    スキャナの概念を導入し、timelineのreducerをすっきりさせました

--- src/redux/reducer/timeline.js
+++ src/redux/reducer/timeline.js
@@ -7,21 +7,22 @@ import {saveTimelines} from '../api/storage';
 
 const initState = [];
 
+const scanner = (state: Array<Timeline>, targetIndex: number, closure: Function): Array<Timeline> => (
+    state.map((item: Timeline, index: number): Timeline => (
+        index === targetIndex ? closure(item) : item
+    ))
+);
+
 export default handleActions({
     [types.UPDATE_CONTENT_TEXT]: (state: Array<Timeline>, action: Object): Array<Timeline> => (
-        state.map((item: Timeline, index: number): Timeline => (
-            index===action.payload.timelineIndex ? item.updateContentText(action.payload.text) : item
-        ))
+        scanner(state, action.payload.timelineIndex, (item: Timeline): Timeline => item.updateContentText(action.payload.text))
     ),
     [types.SET_IN_PROGRESS_STATUS]: (state: Array<Timeline>, action: Object): Array<Timeline> => (
-        state.map((item: Timeline, index: number): Timeline => (
-            index === action.payload.timelineIndex ? item.setInProgress(action.payload.status) : item
-        ))
+        scanner(state, action.payload.timelineIndex, (item: Timeline): Timeline => item.setInProgress(action.payload.status))
     ),
     [types.SET_TIMELINE_MENU]: (state: Array<Timeline>, action: Object): Array<Timeline> => (
-        state.map((item: Timeline, index: number): Timeline => (
-            index === action.payload.timelineIndex ? item.setMenu(action.payload.anchorEl) : item
-        ))
+        scanner(state, action.payload.timelineIndex, (item: Timeline): Timeline => item.setMenu(action.payload.anchorEl))
+    ),
     ),
     [types.ADD_TIMELINE]: (state: Array<Timeline>, action: Object): Array<Timeline> => {
         const nextState = [...state, new Timeline(action.payload.accountIndex, action.payload.timelineType)];
