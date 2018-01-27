9ae474dd3d1bca7c9170a25d470d8e9f0913d36d
on_origami <yonosamochi@me.com>
2018-01-27 17:16:26 +0900

    react-listでContentViewを表示するように

--- src/component/TimelineView/TimelineComponents/ContentView/ContentList.jsx
+++ src/component/TimelineView/TimelineComponents/ContentView/ContentList.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import ReactList from 'react-list';
 import {onlyUpdateForKeys} from 'recompose';
 import {withStyles} from 'material-ui/styles';
 import Divider from 'material-ui/Divider'
@@ -26,6 +27,48 @@ type Props = {
     setReply: Function,
 };
 
+class ContentList extends React.PureComponent<Props> {
+    constructor(props: Props) {
+        super(props);
+        this.renderItem = this.renderItem.bind(this);
+    }
+
+    renderItem(index, key) {
+        console.log(key);
+        const renderTarget = this.props.contents[index];
+        const props = this.props;
+        return (
+            renderTarget instanceof ContentObject ? <div key={key}>
+                <Content
+                    service={props.service}
+                    timelineIndex={props.timelineIndex}
+                    ownerIndex={props.ownerIndex}
+                    data={renderTarget}
+                    callApi={props.callApi}
+                    setReply={props.setReply} />
+                <Divider />
+            </div>:
+            <div key={index}>
+                <Event data={renderTarget} />
+                <Divider />
+            </div>
+        )
+    }
+
+    render() {
+        const props = this.props;
+        return (
+            <div className={props.classes.root}>
+                <ReactList
+                    itemRenderer={this.renderItem}
+                    length={this.props.contents.length}/>
+            </div>
+        )
+    }
+}
+
+/*
+
 const ContentList = onlyUpdateForKeys(['contents'])((props: Props) => (
     <div className={props.classes.root}>
         {props.contents.map((item, index) => (
@@ -47,5 +90,7 @@ const ContentList = onlyUpdateForKeys(['contents'])((props: Props) => (
         ))}
     </div>
 ));
+
+*/
 
 export default withStyles(styles)(ContentList);
