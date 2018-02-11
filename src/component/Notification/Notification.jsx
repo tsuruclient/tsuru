760239eb9242c7ae9545b4d605dd1df828f383eb
on_origami <yonosamochi@me.com>
2018-02-11 21:03:46 +0900

    NotificationとなるSnackbarのモックを作成

--- src/component/Notification/Notification.jsx
+++ src/component/Notification/Notification.jsx
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import {withStyles} from 'material-ui/styles';
+import Snackbar from 'material-ui/Snackbar';
+import IconButton from 'material-ui/IconButton';
+import CloseIcon from 'material-ui-icons/Close';
 
 type Props = {
     classes: Object,
@@ -11,10 +14,30 @@ const styles = theme => ({
     root: {
 
     }
-})
+});
 
 const Notification = (props: Props) => (
-    <div></div>
+    <Snackbar
+        anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+        }}
+        open={true}
+        autoHideDuration={2000}
+        onClose={null}
+        SnackbarContentProps={{
+            'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">はいじゃないが</span>}
+        action={[
+            <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={null} >
+                <CloseIcon />
+            </IconButton>,
+    ]} />
 );
 
 export default withStyles(styles)(Notification);
