2b3b468871daefca4666600e6be5fa0c6203f687
on_origami <yonosamochi@me.com>
2018-03-03 19:55:08 +0900

    styled-componentsを使って書ける所はそっちを使いましょう

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,41 +1,43 @@
 // @flow
 
 import React from 'react';
-import {MuiThemeProvider, createMuiTheme, withStyles} from 'material-ui/styles';
+import {pure, lifecycle} from 'recompose';
+import styled from 'styled-components';
+import {MuiThemeProvider} from 'material-ui/styles';
 
 import Sidebar from '../container/Sidebar';
 import TimelineView from '../container/TimelineView';
 import Dialog from '../container/Dialog';
 
 type Props = {
-    classes: Object,
     theme: Object,
     initApp: Function,
 };
 
-const styles = (theme: Object): any => ({
-    root: {
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        flexDirection: "row",
-        backgroundColor: theme.palette.background.default,
-    },
-});
-
+const Main = styled.div`
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: row;
+        background-color: ${props => props.theme.palette.background.default};
+`;
 
 const App = (props: Props) => {
-    props.initApp();
-    console.log(props.theme.palette.background.default);
     return (
         <MuiThemeProvider theme={props.theme}>
-            <div className={props.classes.root}>
+            <Main theme={props.theme}>
                 <Sidebar />
                 <TimelineView />
-            </div>
+            </Main>
             <Dialog />
         </MuiThemeProvider>
     )
 };
 
-export default withStyles(styles)(App);
+const AppComponent = lifecycle({
+    componentWillMount(){
+        this.props.initApp();
+    }
+})(App);
+
+export default AppComponent;
