bb86992ac346dae6a0ffa82ec22d71bf5ee403a3
ONOUE Origami <yonosamochi@me.com>
2018-03-05 03:33:24 +0900

    わかりません とにかく消すファイルを列挙

---
+++ build-script/delete_files.js
@@ -0,0 +1,20 @@
+module.exports = {
+    app: [
+        '.idea',
+        '.vscode',
+        'build-script',
+        'images',
+        '.editorconfig',
+        '.env',
+        '.flowconfig',
+        '.gitignore',
+        'build.js',
+        'create-react-app-readme.md',
+        'Procfile',
+        'src/component',
+        'src/container',
+        'src/core',
+        'src/helper',
+        'src/redux',
+    ]
+};
--- build-script/release_directory.js
+++ build-script/release_directory.js
@@ -1,1 +1,8 @@
-module.exports = './release';
+module.exports = {
+    release_directory: './release',
+    app_dir: {
+        win: 'Tsuru-win32-x64/resources/app/',
+        mac: 'Tsuru-mas-x64/Tsuru.app/Contents/Resources/app/',
+        linux: 'Tsuru-linux-x64/resources/app/',
+    }
+};
