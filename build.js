fd9307cbdc52153f6ce9e999c88e5173306710b3
ONOUE Origami <yonosamochi@me.com>
2018-03-07 02:02:46 +0900

    謎のビルドスクリプトらしきものを書く

--- build.js
+++ build.js
@@ -1,3 +1,18 @@
+const promisify = require("es6-promisify");
+const exec = promisify(require('child_process').exec);
+require('colors');
+
 const file_delete = require('./build-script/file_delete');
 
-file_delete();
+exec('npm run build').then((stats) => {
+    console.log(stats);
+    console.log('"build" succeeded.\n\n now executed "package"'.blue);
+    return exec('npm run package');
+}).then((stats) => {
+    console.log(stats);
+    console.log('"package" succeeeded.\n'.blue);
+}).catch((err) => {
+    console.error("error occurred.".red);
+    console.error(err);
+});
+
