b3698d41e352bc19c6f4a6c6f0dca0c18d547fa5
ONOUE Origami <yonosamochi@me.com>
2018-03-11 23:57:53 +0900

    無 になりました

--- build-script/build.js
+++ build-script/build.js
@@ -2,8 +2,6 @@ const promisify = require("es6-promisify");
 const exec = promisify(require('child_process').exec);
 require('colors');
 
-const file_delete = require('./build-script/file_delete');
-
 exec('npm run build').then((stats) => {
     console.log(stats);
     console.log('"build" succeeded.\n\n now executed "package"'.blue);
--- build-script/file_delete.js
+++ build-script/file_delete.js
@@ -1,8 +1,3 @@
 const fs = require('fs');
-const release_directory = require('./release_directory');
 
-module.exports = () => {
-    fs.readdir(release_directory, (err, files) => {
-        console.log(files.forEach((file) => {console.log(file)}));
-    });
-};
+module.exports = undefined; // 無
