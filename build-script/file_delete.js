876e53c5d5136c5ad47bd8cfcd9599a94075b9f0
ONOUE Origami <yonosamochi@me.com>
2018-03-12 01:32:27 +0900

    ファイルを消したりするfile_delete.jsを整備

--- build-script/file_delete.js
+++ build-script/file_delete.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
+const del = require('del');
+const files = require('./files');
+require('colors');
 
-module.exports = undefined; // 無
+module.exports = () => {
+    Object.keys(files.file).forEach(keys => {
+        try{
+            fs.statSync(files.release_directory + files.app_dir[keys]);
+            del(files.file[keys]).then(paths => {
+                console.log(('Unnecessary file of ' + keys + ' release build was successfully deleted.').blue);
+            });
+        }catch(e){
+            console.log((keys + " release build isn't exists.").red);
+        }
+    });
+};
