5c21d2a8ee486fecdb827e98586889cf15ebd7a0
ONOUE Origami <yonosamochi@me.com>
2018-03-13 02:15:30 +0900

    compressorの中身がない

--- build-script/compressor.js
+++ build-script/compressor.js
@@ -1,3 +1,14 @@
 const AdmZip = require('adm-zip');
 const targz = require('targz');
+const platforms = require('./platform');
 
+return (platform) => {
+    new Promise((resolve, reject) => {
+        if(platform === platforms.linux){
+            // tar.gz
+        }else{
+            // zip
+        }
+        resolve();
+    });
+};
