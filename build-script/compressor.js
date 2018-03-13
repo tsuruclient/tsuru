e920e8ca9c1097fac4fea55ec040409abc76ee09
on_origami <yonosamochi@me.com>
2018-03-13 18:57:14 +0900

    ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい ごめんなさい

--- build-script/compressor.js
+++ build-script/compressor.js
@@ -2,7 +2,7 @@ const AdmZip = require('adm-zip');
 const targz = require('targz');
 const platforms = require('./platform');
 
-return (platform) => {
+module.exports = (platform) => {
     new Promise((resolve, reject) => {
         if(platform === platforms.linux){
             // tar.gz
