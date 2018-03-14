977f78712d9d66a90e330d8d3a22680e2dbbc35a
on_origami <yonosamochi@me.com>
2018-03-14 10:16:49 +0900

    compressor.jsを実際に動くように

--- build-script/compressor.js
+++ build-script/compressor.js
@@ -1,14 +1,36 @@
 const AdmZip = require('adm-zip');
 const targz = require('targz');
 const platforms = require('./platform');
+const files = require('./files');
+
+const compress_targz = (platform) => {
+    return new Promise((resolve, reject) => {
+        targz.compress({
+            src: files.release_directory + files.release_dir[platform],
+            dest: files.release_directory + files.release_dir[platform] + '.tar.gz'
+        }, function(err){
+            if(err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    })
+};
+
+const compress_zip = (platform) => {
+    return new Promise((resolve, reject) => {
+        let zip = new AdmZip();
+        zip.addLocalFolder(files.release_directory + files.release_dir[platform]);
+        zip.writeZip(files.release_directory + files.release_dir[platform] + '.zip');
+        resolve();
+    })
+};
 
 module.exports = (platform) => {
-    new Promise((resolve, reject) => {
-        if(platform === platforms.linux){
-            // tar.gz
-        }else{
-            // zip
-        }
-        resolve();
-    });
+    if(platform === platforms.linux){
+        return compress_targz(platform);
+    }else{
+        return compress_zip(platform);
+    }
 };
