e4458ef96bd882160b6d5e295a4572edaee68cfb
on_origami <yonosamochi@me.com>
2018-03-16 17:58:59 +0900

    travisのdeploy fileを微修正、build scriptが正常にすべてzipにせず終了するバグを修正

--- build-script/exec_platform.js
+++ build-script/exec_platform.js
@@ -7,24 +7,22 @@ const compressor = require('./compressor');
 
 module.exports = () => {
     return new Promise((resolve, reject) => {
-        Object.keys(platforms).forEach((platform) => {
+        Object.keys(platforms).forEach((platform, index, array) => {
             try {
                 fs.statSync(files.release_directory + files.app_dir[platform]);
                 del(files.file[platform]).then(paths => {
-                    console.log(('Unnecessary file of ' + platform + ' release build was successfully deleted.').blue);
                     return asar_packing(platform);
                 }).then(paths => {
-                    console.log(('successfully '+ platform +' asar!').blue);
                     return compressor(platform);
                 }).then(() => {
-                    console.log(('successfully ' + platform + ' compressed application!').blue);
-                    resolve();
+                    console.log((platform + ' build succeeded!').blue);
+                    if(array.length === index) resolve();
                 }).catch(e => {
-                    throw e;
+                    if(array.length === index) resolve();
                 });
             } catch (e) {
-                console.log((platform + " release build isn't exists.").red);
+                console.log((platform + " release build does't exists.").red);
             }
-        })
+        });
     })
 };
