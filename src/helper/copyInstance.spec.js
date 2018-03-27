f37fd8c0f8bd7c4a067f687bfca043c6e1acacbc
FirstStar <FirstStar@users.noreply.github.com>
2018-03-28 02:03:52 +0900

    helper/copyInstanceのテストを導入

---
+++ src/helper/copyInstance.spec.js
@@ -0,0 +1,21 @@
+import copyInstance from './copyInstance';
+
+
+
+describe('copyInstance()', ()=>{
+    it('equally instance method', () => {
+        class test{
+            constructor(value){
+                this.value = value;
+                this.array = [value, value*2, value+'tostring']
+            }
+
+            testfunc(){
+                return this.value;
+            }
+        }
+
+        let source  = new test(44532);
+        expect(copyInstance(source).testfunc === source.testfunc).toBe(true);
+    })
+});
