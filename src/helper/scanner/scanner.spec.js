07f75d9f19ae0060b3996fc27015b1779d7a65fe
FirstStar <FirstStar@users.noreply.github.com>
2018-03-28 20:52:07 +0900

    testのタイトル忘れてました

--- src/helper/scanner/scanner.spec.js
+++ src/helper/scanner/scanner.spec.js
@@ -3,7 +3,7 @@ import scanner from './scanner';
 const state = ['t', 'e', 's', 't'];
 
 describe('scanner()', () => {
-    it('', () => {
+    it('scan and simple-clojure', () => {
         expect(scanner(state, 0, (item) => ('j'))).toEqual(['j', 'e', 's', 't'])
     })
 });
