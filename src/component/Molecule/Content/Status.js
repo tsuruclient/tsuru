803252cce9b039345bc6b93f37bf2ef03e71a5d1
FirstStar <FirstStar@users.noreply.github.com>
2018-04-03 11:02:11 +0900

    Statusを作成

---
+++ src/component/Molecule/Content/Status.js
@@ -0,0 +1,52 @@
+// @flow
+import React from 'react';
+import { pure } from 'recompose';
+import styled from 'styled-components';
+import Avatar from 'material-ui/Avatar';
+import Typography from 'material-ui/Typography';
+
+const Styled = {
+    Root: styled.div`
+        display: flex;
+        flex-direction: column;
+    `,
+    Body: styled.div`
+        display: flex;
+    `,
+    Text: styled.div`
+    `,
+    Bottom: styled.div`
+    `
+};
+
+type Props = {
+    data: Object,
+    handler: Object,
+}
+
+const Common = (props: Props) => (
+    <Styled.Root>
+        <Styled.Body>
+            <Avatar
+                src={props.data.user.avatar}
+                onClick={null}
+                onDoubleClick={null}/>
+            <Styled.Text>
+                <Typography
+                    variant="caption"
+                    onClick={null}
+                    onDoubleClick={null}>
+                    {props.data.user.displayName + "@"+ props.data.user.screenName}
+                </Typography>
+                <Typography
+                    variant="body1"
+                    onClick={null}
+                    onDoubleClick={null}>
+                    {props.data.content}
+                </Typography>
+            </Styled.Text>
+        </Styled.Body>
+    </Styled.Root>
+);
+
+export default pure(Common);
