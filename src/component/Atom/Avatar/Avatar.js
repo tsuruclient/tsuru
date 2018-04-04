0e9d168c244c2d092366fbf59a67e391c91c4a7a
ONOUE Origami <yonosamochi@me.com>
2018-04-04 23:48:52 +0900

    Avatarからpadding-rightを削除

--- src/component/Atom/Avatar/Avatar.js
+++ src/component/Atom/Avatar/Avatar.js
@@ -5,7 +5,6 @@ import Avatar from 'material-ui/Avatar';
 
 const Styled = {
     Avatar: styled(Avatar)`
-        padding-right: 4px;
         width: ${(props: Props) => props.styles.size + 4}px !important;
         height: ${(props: Props) => props.styles.size}px !important;
         border-radius: ${(props: Props) => props.styles.rect ? 10 : 50}% !important;
