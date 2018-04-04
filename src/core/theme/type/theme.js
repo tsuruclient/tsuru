630ee64e08be4345530c167a44fa379148ff66f2
ONOUE Origami <yonosamochi@me.com>
2018-04-05 01:51:52 +0900

    中途半端ですがtheme object typeを作成

---
+++ src/core/theme/type/theme.js
@@ -0,0 +1,34 @@
+// @flow
+export type themeObject = {
+    atom: {
+        icon: {
+            size: number,
+            color: {
+                gray: string,
+                colorful: {
+                    reply: string,
+                    favorite: string,
+                    repeat: string,
+                }
+            }
+        },
+    },
+    molecule: {
+    },
+    organism: {
+        panel: {
+            backgroundColor: string,
+            width: number,
+            statusBar: {
+                height: number,
+                color: {
+                    gray: string,
+                    colorful: {
+                        posting: string,
+                        streaming: string,
+                    }
+                }
+            }
+        }
+    }
+}
