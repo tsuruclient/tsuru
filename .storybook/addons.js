7035c455e132eaef99c3df704b7faf8104b8bbba
ONOUE Origami <yonosamochi@me.com>
2018-04-21 11:56:32 +0900

    アドオンの並びを変えた

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,6 +2,6 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-events/register';
 import '@storybook/addon-knobs/register'
+import '@storybook/addon-notes/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-jest/register';
-import '@storybook/addon-notes/register';
