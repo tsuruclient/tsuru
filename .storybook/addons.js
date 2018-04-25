83cf1ff24da34b91fb3ae64065e92ddee7626348
FirstStar <FirstStar@users.noreply.github.com>
2018-04-25 17:49:20 +0900

    意味がないけどstorysourceを再導入

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,8 @@
-import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/addon-events/register';
-import '@storybook/addon-knobs/register'
 import '@storybook/addon-notes/register';
+import '@storybook/addon-knobs/register'
+import '@storybook/addon-actions/register';
+import '@storybook/addon-events/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-backgrounds/register';
 import '@storybook/addon-jest/register';
