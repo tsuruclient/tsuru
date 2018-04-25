aa45ea9e82bf1f922e4e675f7d124e60c3a9a81d
FirstStar <FirstStar@users.noreply.github.com>
2018-04-25 17:10:20 +0900

    IconをButton化するHoCを作成

--- src/component/Atom/Button/IconButton/IconButtonBase.js
+++
@@ -1,7 +0,0 @@
-//@flow
-import React from 'react';
-import styled from 'styled-components';
-
-type Props = {
-
-}
---
+++ src/component/Atom/Button/generateIconButton.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import ButtonBase from 'material-ui/ButtonBase';
+
+type Props = {
+    onclick: Function,
+};
+
+export default (Component: Object, props: Props) => (
+    <ButtonBase onClick={props.onclick}>
+        <Component/>
+    </ButtonBase>
+);
